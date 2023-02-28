bac9257e3afd6b682c6c41b43cc5788bb908ecf5
ZeKJ <kennethjacobsen@hotmail.com>
2023-02-28 14:54:03 +0100

    Test af vite

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Dataordbog',
+  description: 'Just playing around.',
+  srcDir: './',
+  //base: '/repo/',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ],
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,10 @@
+Overblik over data som benyttes i datahubben
+
+- [Formål]
+- [Templates]
+- [Attributer]
+- [Egenskaber]
+- [Egenskabssæt]
+- [Værdilister]
+- [Dokumenter]
+- [Opgaver]
