86f18e03eed7bef920a636ff4561c34fbc3486b9
ZeKJ <kennethjacobsen@hotmail.com>
2023-04-13 12:07:00 +0200

    Deeplink

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -138,7 +138,7 @@ export default {
     nav: [
       {
         text: "Dokumentation",
-        link: "https://fmdatahub.github.io/Documentation/",
+        link: "https://fmdatahub.github.io/Documentation/Datahub/Integrationer/API/Routers/Datadictionary/",
         rel: false,
       },
     ],
